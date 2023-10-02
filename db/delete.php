ae606ce58d3e37c7ab4dea9ed9a94dc3d3dc2eb0
threehHong <honghyohun@naver.com>
2023-10-03 00:44:16 +0900

    feat: 삭제시 index값 초기화 query문 추가

--- db/delete.php
+++ db/delete.php
@@ -11,9 +11,19 @@
     echo "Error: " . $query . "<br>" . $conn->error;
   }
 
+  // AUTO_INCREMENT 초기화
+  $query_reset_auto_increment = "ALTER TABLE Expense_Tracker AUTO_INCREMENT = 1";
+  $conn->query($query_reset_auto_increment);
+
+  // idx 값 업데이트
+  $query_update_idx = "SET @COUNT = 0";
+  $conn->query($query_update_idx);
+  $query_update_idx = "UPDATE Expense_Tracker SET idx = @COUNT:=@COUNT+1";
+  $conn->query($query_update_idx);
+
   echo "<script>
             location.href='../index.php'
         </script>";
 
-  /* $conn->close(); */
+  $conn->close();
 ?>
