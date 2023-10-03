1467e1afb526f5630a43c383c6453f8158242606
threehHong <honghyohun@naver.com>
2023-10-03 19:26:01 +0900

    feat: edit.db에 숫자가 아닌 문자 제거 정규식 추가

--- db/edit.php
+++ db/edit.php
@@ -6,7 +6,10 @@
     $item = $_POST['item'];
     $amount = $_POST['amount'];
 
-    echo $idx[0]."".$date[0]."".$item[0]."".$amount[0];
+    /* echo $idx[0]."".$date[0]."".$item[0]."".$amount[0]; */
+
+    // 숫자가 아닌 문자 제거
+    $amount = preg_replace('/[^0-9]/', '', $amount);
  
     $query = "UPDATE Expense_Tracker set date='$date[0]', item='$item[0]', amount='$amount[0]' where idx='{$idx[0]}'";
 
