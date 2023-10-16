01beb0adb872fa3c860cac1f97090b4270edee26
threehHong <honghyohun@naver.com>
2023-10-16 14:51:24 +0900

    refactor: 코드 depth 수정(4 -> 2)

--- database/edit.php
+++ database/edit.php
@@ -1,25 +1,25 @@
 <?php
-    include "../config/db_connect.php";
+  include "../config/db_connect.php";
 
-    $idx = $_POST['idx'];
-    $date = $_POST['date'];
-    $item = $_POST['item'];
-    $amount = $_POST['amount'];
+  $idx = $_POST['idx'];
+  $date = $_POST['date'];
+  $item = $_POST['item'];
+  $amount = $_POST['amount'];
 
-    /* echo $idx[0]."".$date[0]."".$item[0]."".$amount[0]; */
+  /* echo $idx[0]."".$date[0]."".$item[0]."".$amount[0]; */
 
-    // 숫자가 아닌 문자 제거
-    $amount = preg_replace('/[^0-9]/', '', $amount);
- 
-    $query = "UPDATE Expense_Tracker set date='$date[0]', item='$item[0]', amount='$amount[0]' where idx='{$idx[0]}'";
+  // 숫자가 아닌 문자 제거
+  $amount = preg_replace('/[^0-9]/', '', $amount);
 
-    global $conn;
+  $query = "UPDATE Expense_Tracker set date='$date[0]', item='$item[0]', amount='$amount[0]' where idx='{$idx[0]}'";
 
-    if ($conn->query($query) !== TRUE) {
-        echo "Error: " . $query . "<br>" . $conn->error;
-    }
+  global $conn;
 
-    echo "<script>
-            location.href='../index.php'
+  if ($conn->query($query) !== TRUE) {
+      echo "Error: " . $query . "<br>" . $conn->error;
+  }
+
+  echo "<script>
+          location.href='../index.php'
           </script>";
 ?>
