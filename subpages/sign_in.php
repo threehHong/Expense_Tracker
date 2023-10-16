6c59775056bd351be6e1a47bdabbeba386ef58fc
threehHong <honghyohun@naver.com>
2023-10-16 16:04:12 +0900

    feat: 회원가입 프론트 페이지 추가

---
+++ subpages/sign_in.php
@@ -0,0 +1,54 @@
+<?php
+  include "../config/db_connect.php";
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./assets/css/common.css">
+
+    <link rel="stylesheet" href="../assets/css/sign_in.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Sign in</title>
+  </head>
+  <body>
+    <div id="wrap">
+      <div class="form_wrap">
+        <form action="">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">ID</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <div class="btn_sign_up_path">
+            <a href=""> 회원가입 </a>
+          </div>
+
+          <div class="validation_result">
+            <p>
+              유효성 검사 결과 표시
+            </p>
+          </div>
+
+          <div class="btn_sign_in">
+            <button type="submit" class="btn btn-primary"> 로그인 </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
