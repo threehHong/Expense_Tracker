cc0e62dcaecf6620ca1e945365ea6838a58821f0
threehHong <honghyohun@naver.com>
2023-10-22 15:30:09 +0900

    feat: 아이디, 비밀번호, 이메일 검증 기능 추가

--- subpages/sign_up.php
+++ subpages/sign_up.php
@@ -13,7 +13,7 @@ include "../config/db_connect.php";
   <!-- favicon -->
   <link rel="shortcut icon" href="../assets/images/logo/favicon.svg" type="image/x-icon">
 
-  <link rel="stylesheet" href="./assets/css/common.css">
+  <link rel="stylesheet" href="../assets/css/common.css">
 
   <link rel="stylesheet" href="../assets/css/account.css">
 
@@ -37,31 +37,31 @@ include "../config/db_connect.php";
         </div>
 
         <div class="form-floating mb-3">
-          <input type="id" name="id" class="form-control signup_id" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingInput">ID</label>
-          <div class="validation_message"> validation message </div>
+          <input type="id" name="id" class="form-control signup_id" id="signup_id" placeholder="ID">
+          <label for="signup_id">ID</label>
+          <div class="validation_message"></div>
         </div>
 
         <div class="form-floating">
-          <input type="password" name="password" class="form-control signup_password" id="floatingPassword" placeholder="Password">
-          <label for="floatingPassword">Password</label>
-          <div class="validation_message"> validation message </div>
+          <input type="password" name="password" class="form-control signup_password" id="signup_password" placeholder="Password">
+          <label for="signup_password">Password</label>
+          <div class="validation_message"></div>
         </div>
 
         <div class="form-floating">
-          <input type="password" class="form-control signup_password_check" id="floatingPassword" placeholder="Password">
-          <label for="floatingPassword">Password Check</label>
-          <div class="validation_message"> validation message </div>
+          <input type="password" class="form-control signup_password_check" id="signup_password_check" placeholder="Password">
+          <label for="signup_password_check">Password Check</label>
+          <div class="validation_message"></div>
         </div>
 
         <div class="form-floating email">
-          <input type="email" class="form-control" id="floatingEmail" placeholder="Password">
+          <input type="email" class="form-control signup_email" id="floatingEmail" placeholder="Password">
           <label for="floatingEmail">Email</label>
-          <button class="btn btn-outline-secondary" type="button">인증</button>
-          <div class="validation_message"> validation message </div>
+          <button class="btn btn-outline-secondary signup_email_verify_btn" type="button">인증</button>
+          <div class="validation_message"></div>
         </div>
 
-        <div class="form-floating">
+        <div class="form-floating verification_code">
           <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
           <label for="floatingPassword">인증 코드</label>
         </div>
@@ -75,15 +75,70 @@ include "../config/db_connect.php";
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script>
+    const signupIdInput = $(".signup_id");
+    const signupPasswordInput = $(".signup_password");
+    const signupPasswordInputCheck = $(".signup_password_check");
+    const emailInput = $(".signup_email");
+    const emailVerifyBtn = $(".signup_email_verify_btn");
+    const emailVerificationCode = $(".verification_code");
+
+    const idRegEx = /^[a-zA-Z0-9]{6,}$/
+    const passwordRegEx = /^.{6,}$/;
+    const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    signupIdInput.on("blur", function() {
+      if (!idRegEx.test(signupIdInput.val())) {
+        signupIdInput.siblings('.validation_message').text('6자 이상 문자나 숫자를 입력하세요');
+      } else {
+        signupIdInput.siblings('.validation_message').text('');
+      }
+    });
+
+    signupPasswordInput.on("blur", function() {
+      if (!passwordRegEx.test(signupPasswordInput.val())) {
+        signupPasswordInput.siblings('.validation_message').text('6자 이상 입력하세요');
+      } else {
+        signupPasswordInput.siblings('.validation_message').text('');
+      }
+    });
+
+    signupPasswordInputCheck.on("blur", function() {
+      if (signupPasswordInput.val() !== signupPasswordInputCheck.val()) {
+        signupPasswordInputCheck.siblings('.validation_message').text('비밀번호 불일치');
+      } else {
+        signupPasswordInputCheck.siblings('.validation_message').text('');
+      }
+    });
+
+    emailInput.on("blur", function() {
+      if (!emailRegEx.test(emailInput.val())) {
+        emailInput.siblings('.validation_message').text('이메일 형식 불일치');
+      } else {
+        emailInput.siblings('.validation_message').text('');
+      }
+    });
+
+    emailVerifyBtn.on("click", function() {
+      if (emailRegEx.test(emailInput.val())) {
+        console.log("출력 확인")
+        /* emailVerifyBtn.prop("disabled", true); */
+        emailVerificationCode.css({
+          "display": "block",
+          "opacity": "1",
+          "transition": "1s",
+        });
+      }
+      /* else {
+             emailInput.siblings('.validation_message').text('');
+           } */
+    })
+
     function onSubmitCheck(e) {
 
-      /* e.preventDefault();
+      e.preventDefault();
 
-      const signupIdInput = $(".signup_id").val();
-      const signupPasswordInput = $(".signup_password").val();
-      const signupPasswordInputCheck = $(".signup_password_check").val();
+      console.log(signupIdInput, signupPasswordInput, signupPasswordInputCheck)
 
-      console.log(signupIdInput, signupPasswordInput, signupPasswordInputCheck) */
 
       /* validateId(signinIdInput, signinPasswordInput); */
 
@@ -95,14 +150,13 @@ include "../config/db_connect.php";
 
     }
 
-    /* function validateId(signinIdInput, signinPasswordInput) {
+    function validateId(signupIdInput, signinPasswordInput) {
       $.ajax({
         type: 'post',
-        url: '../database/account/sign_in_validation.php',
+        url: '../database/account/sign_up_validation.php',
         data: {
           signinIdInput: signinIdInput,
-          signinPasswordInput,
-          signinPasswordInput
+          signupPasswordInput: signupPasswordInput
         },
         dataType: 'json',
         error: function() {
@@ -120,7 +174,7 @@ include "../config/db_connect.php";
 
         }
       })
-    } */
+    }
   </script>
 </body>
 
