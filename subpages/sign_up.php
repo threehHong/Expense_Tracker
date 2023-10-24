a79fdec8387ec63ca38da309fbb849b3de0ed5a5
threehHong <honghyohun@naver.com>
2023-10-24 18:03:41 +0900

    feat: 이메일 인증 기능 추가

--- subpages/sign_up.php
+++ subpages/sign_up.php
@@ -29,7 +29,7 @@ include "../config/db_connect.php";
 <body>
   <div id="wrap">
     <div class="form_wrap">
-      <form action="../database/account/sign_up_validation.php" method="POST" onsubmit="onSubmitCheck(event)">
+      <form action="../database/account/sign_up_db.php" id="sign_up" method="POST">
         <div class="logo">
           <a href="../index.php">
             <img src="../assets/images/logo/Logo.svg" alt="Logo">
@@ -49,13 +49,13 @@ include "../config/db_connect.php";
         </div>
 
         <div class="form-floating">
-          <input type="password" class="form-control signup_password_check" id="signup_password_check" placeholder="Password">
+          <input type="password" name="password_check" class="form-control signup_password_check" id="signup_password_check" placeholder="Password">
           <label for="signup_password_check">Password Check</label>
           <div class="validation_message"></div>
         </div>
 
         <div class="form-floating email">
-          <input type="email" class="form-control signup_email" id="floatingEmail" placeholder="Password">
+          <input type="email" name="email" class="form-control signup_email" id="floatingEmail" placeholder="Password">
           <label for="floatingEmail">Email</label>
           <button class="btn btn-outline-secondary signup_email_verify_btn" type="button">인증</button>
           <div class="validation_message"></div>
@@ -68,7 +68,7 @@ include "../config/db_connect.php";
         </div>
 
         <div class="btn_account sign_up">
-          <button type="submit" class="btn btn-primary" disabled> 회원가입 </button>
+          <button type="submit" name="submitButton" class="btn btn-primary" disabled> 회원가입 </button>
         </div>
       </form>
     </div>
@@ -94,9 +94,13 @@ include "../config/db_connect.php";
     let isEmailValid = false;
     let isVerificationCodeValid = false;
 
+    let verificationCode;
+
     signupIdInput.on("input", function() {
       if (!idRegEx.test($(this).val())) {
-        $(this).siblings('.validation_message').text('6자 이상 문자나 숫자를 입력하세요');
+        $(this).siblings('.validation_message').text('6자 이상 영문이나 숫자를 입력하세요');
+        isIdValid = false;
+        checkAllValid()
       } else {
         $(this).siblings('.validation_message').text('');
         isIdValid = true;
@@ -109,7 +113,7 @@ include "../config/db_connect.php";
 
       $.ajax({
         type: 'post',
-        url: '../database/account/sign_up_validation.php',
+        url: '../database/account/sign_up_validation_ajax.php',
         data: {
           signupIdInputValue: signupIdInputValue,
         },
@@ -118,10 +122,11 @@ include "../config/db_connect.php";
           console.log('error');
         },
         success: function(response) {
-          if (response.user_id_check) {
+          if (response.user_id_check === "아이디 중복") {
             signupIdInput.siblings('.validation_message').text('중복된 아이디 입니다.');
+            isIdValid = true;
+            checkAllValid()
           } else {
-            /* signupIdInput.siblings('.validation_message').text(''); */
             isIdValid = true;
             checkAllValid()
           }
@@ -132,6 +137,8 @@ include "../config/db_connect.php";
     signupPasswordInput.on("input", function() {
       if (!passwordRegEx.test($(this).val())) {
         $(this).siblings('.validation_message').text('6자 이상 입력하세요');
+        isPasswordValid = false;
+        checkAllValid()
       } else {
         $(this).siblings('.validation_message').text('');
         isPasswordValid = true;
@@ -142,6 +149,8 @@ include "../config/db_connect.php";
     signupPasswordInputCheck.on("input", function() {
       if (signupPasswordInput.val() !== $(this).val()) {
         $(this).siblings('.validation_message').text('비밀번호 불일치');
+        isPasswordCheckValid = false;
+        checkAllValid()
       } else {
         $(this).siblings('.validation_message').text('');
         isPasswordCheckValid = true;
@@ -152,6 +161,8 @@ include "../config/db_connect.php";
     emailInput.on("input", function() {
       if (!emailRegEx.test($(this).val())) {
         $(this).siblings('.validation_message').text('이메일 형식 불일치');
+        isEmailValid = false;
+        checkAllValid()
       } else {
         $(this).siblings('.validation_message').text('');
         isEmailValid = true;
@@ -162,52 +173,61 @@ include "../config/db_connect.php";
     emailVerifyBtn.on("click", function() {
       if (!emailInput.val()) {
         emailInput.siblings('.validation_message').text('이메일을 입력하세요');
+        isEmailValid = false;
+        checkAllValid()
       } else if (emailRegEx.test(emailInput.val())) {
-        console.log("출력 확인")
-        /* emailVerifyBtn.prop("disabled", true); */
         emailVerificationCode.css({
           "display": "block",
           "opacity": "1",
           "transition": "1s",
         });
         isEmailValid = true;
-        checkAllValid()
+
+        $.ajax({
+          type: "POST",
+          url: "../database/account/email_verification.php",
+          data: {
+            action: emailInput.val()
+          },
+          success: function(response) {
+            response = JSON.parse(response);
+            verificationCode = response.verification_code;
+          },
+          error: function(error) {
+            console.error(error);
+          }
+        });
       }
     })
 
     emailVerificationCodeInput.on("input", function() {
-      if (emailVerificationCodeInput.val()) {
+      if (emailVerificationCodeInput.val() == verificationCode) {
         isVerificationCodeValid = true;
+        $(this).siblings('.validation_message').text('');
+        checkAllValid()
+      } else {
+        isVerificationCodeValid = false;
+        $(this).siblings('.validation_message').text('코드 불일치');
         checkAllValid()
       }
     })
 
     function checkAllValid() {
       if (isIdValid && isPasswordValid && isPasswordCheckValid && isEmailValid && isVerificationCodeValid) {
+        console.log("true");
         $(".sign_up .btn").removeAttr("disabled");
         $(".sign_up .btn").css({
           "opacity": "1",
           "transition": "1s",
         });
+      } else {
+        console.log("false");
+        $(".sign_up .btn").attr("disabled", true);
+        $(".sign_up .btn").css({
+          "opacity": "0.5",
+          "transition": "1s",
+        });
       }
-    }
-
-
-    function onSubmitCheck(e) {
-
-      e.preventDefault();
-
-      console.log(signupIdInput, signupPasswordInput, signupPasswordInputCheck)
-
-
-      /* validateId(signinIdInput, signinPasswordInput); */
-
-      /* 
-      밑에와 같은 방식으로 리팩토링 하기.
-      if (!validateForm()) e.preventDefault();
-      console.log("출력 확인"); 
-      */
-
     }
   </script>
 </body>
