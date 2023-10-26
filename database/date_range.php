ef7e423c113a135981c3ecada85cd2802ce66881
threehHong <honghyohun@naver.com>
2023-10-26 16:39:27 +0900

    feat: 구간 설정시 회원 아이디가 있을 경우와 없을 경우에 따라 날짜 DB 저장 로직 추가

--- database/date_range.php
+++ database/date_range.php
@@ -1,11 +1,13 @@
 <?php
 include "../config/db_connect.php";
 
+session_start();
+
 if (isset($_GET['startDate'])) {
   $startDate = $_GET['startDate'];
 
   // 데이터가 이미 있는지 확인
-  $checkQuery = "SELECT COUNT(*) as count FROM expense_tracker_date_range WHERE idx = 1";
+  $checkQuery = "SELECT COUNT(*) as count FROM expense_tracker_date_range";
   $result = $conn->query($checkQuery);
 
   if ($result) {
@@ -14,10 +16,15 @@ if (isset($_GET['startDate'])) {
 
     if ($count > 0) {
       // 데이터가 있으면 업데이트
-      $query = "UPDATE expense_tracker_date_range SET start_date = '${startDate}' WHERE idx = 1";
+      if (isset($_SESSION['ID'])) {
+        $user_id = $_SESSION['ID'];
+        $query = "UPDATE expense_tracker_date_range SET start_date = '${startDate}' WHERE user_id = '${user_id}'";
+      } else {
+        $query = "UPDATE expense_tracker_date_range SET start_date = '${startDate}' WHERE user_id IS NULL";
+      }
     } else {
       // 데이터가 없으면 삽입
-      $query = "INSERT INTO expense_tracker_date_range (idx, start_date) VALUES (1, '${startDate}')";
+      $query = "INSERT INTO expense_tracker_date_range (start_date, user_id) VALUES ('${startDate}', '${$user_id}')";
     }
   }
 } else if (isset($_GET['endDate'])) {
@@ -33,14 +40,18 @@ if (isset($_GET['startDate'])) {
 
     if ($count > 0) {
       // 데이터가 있으면 업데이트
-      $query = "UPDATE expense_tracker_date_range SET end_date = '${endDate}' WHERE idx = 1";
+      if (isset($_SESSION['ID'])) {
+        $user_id = $_SESSION['ID'];
+        $query = "UPDATE expense_tracker_date_range SET end_date = '${endDate}' WHERE user_id = '${user_id}'";
+      } else {
+        $query = "UPDATE expense_tracker_date_range SET end_date = '${endDate}' WHERE user_id IS NULL";
+      }
     } else {
       // 데이터가 없으면 삽입
-      $query = "INSERT INTO expense_tracker_date_range (idx, end_date) VALUES (1, '${endDate}')";
+      $query = "INSERT INTO expense_tracker_date_range (end_date, user_id) VALUES ('${endDate}' , '${$user_id}')";
     }
   }
 }
-
 
 global $conn;
 
