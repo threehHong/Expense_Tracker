4b74e40e6f73fa2981f74648bdbb5b63d4a42b8e
threehHong <honghyohun@naver.com>
2023-10-27 16:30:48 +0900

    refactor: 회원가입시 해당 아이디 날짜 구간 현재 날짜랑 현재 날짜 기준 30일 전 날짜로 구간 초기화 로직으로 수정

--- database/account/sign_up_db.php
+++ database/account/sign_up_db.php
@@ -9,8 +9,11 @@ $user_pw = hash('sha512', $user_pw);
 $sql = "INSERT INTO expense_tracker_member (id, password, email) VALUES ('{$user_id}', '{$user_pw}', '{$user_email}')";
 
 /* if (strlen($user_id) > 0) { */
+$currentDate = date("Y-m-d");
+$thirtyDaysAgo = date("Y-m-d", strtotime("-30 days", strtotime($currentDate)));
+
 if ($conn->query($sql) === true) {
-  $sql = "INSERT INTO expense_tracker_date_range (user_id) VALUES ('{$user_id}')";
+  $sql = "INSERT INTO expense_tracker_date_range (user_id, start_date, end_date) VALUES ('{$user_id}', '{$thirtyDaysAgo}', '{$currentDate}')";
   if ($conn->query($sql) === true) {
     echo "<script> 
             location.href = '../../index.php'; 
