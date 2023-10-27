06588491a9147660f2e7ebc78d9683966aa4131a
threehHong <honghyohun@naver.com>
2023-10-27 15:23:57 +0900

    feat: 이메일 중복 확인 쿼리문 추가

--- database/account/sign_up_validation_ajax.php
+++ database/account/sign_up_validation_ajax.php
@@ -1,15 +1,29 @@
 <?php
 include "../../config/db_connect.php";
 
-$user_id = $_POST['signupIdInputValue'];
+if (isset($_POST['signupIdInputValue'])) {
+  $user_id = $_POST['signupIdInputValue'];
 
-$sql = "SELECT * from expense_tracker_member where id='{$user_id}'";
-$result = $conn->query($sql);
+  $sql = "SELECT * from expense_tracker_member where id='{$user_id}'";
+  $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  $row = $result->fetch_object();
-  $response['user_id_check'] = '아이디 중복';
-} else {
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_object();
+    $response['user_id_check'] = '아이디 중복';
+  } else {
+  }
+} else if (isset($_POST['emailInputValue'])) {
+  $email_Input_value = $_POST['emailInputValue'];
+
+  $sql = "SELECT * from expense_tracker_member where email='{$email_Input_value}'";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_object();
+    $response['user_email_check'] = '이메일 중복';
+  } else {
+    $response['user_email_check'] = '';
+  }
 }
 
 echo json_encode($response);
